40991bd6eb1070049982ad27dd6e5a0f6d66ae74
Geoffrey Delenne <geoffrey.delenne@gmail.com>
2021-01-28 09:20:27 +0100

    Refacto of repositories

--- src/Adapter/Repository/Character/MarvelApiCharacterRepositoryAdapter.php
+++ src/Adapter/Repository/Character/MarvelApiCharacterRepositoryAdapter.php
@@ -2,26 +2,54 @@
 
 namespace App\Adapter\Repository\Character;
 
+use App\DTO\Character;
 use App\HttpClient\SymfonyHttpClient;
+use App\Service\CharacterFactory;
 use DateTime;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class MarvelApiCharacterRepositoryAdapter implements CharacterRepositoryAdapterInterface
 {
     private SymfonyHttpClient $client;
+    private CharacterFactory $characterFactory;
     private string $baseUrl;
     private string $apiPublicKey;
     private string $apiPrivateKey;
 
-    public function __construct(SymfonyHttpClient $client, ParameterBagInterface $params)
+    public function __construct(SymfonyHttpClient $client, CharacterFactory $characterFactory, ParameterBagInterface $params)
     {
         $this->client = $client;
+        $this->characterFactory = $characterFactory;
         $this->baseUrl = $params->get('marvel_url');
         $this->apiPublicKey = $params->get('marvel_public_key');
         $this->apiPrivateKey = $params->get('marvel_private_key');
     }
 
-    public function getCharacters(int $limit = 1, int $offset = 0, int $id = null): array
+    /**
+     * @return Character[]
+     */
+    public function getMarvelCharacters(int $limit = 1, int $offset = 0, int $id = null): array
+    {
+        $charactersRawData = $this->getCharactersFromApi($limit, $offset, $id);
+
+        return $this->getCharacterFromRawData($charactersRawData);
+    }
+
+    /**
+     * @return Character[]
+     */
+    public function getCharacterFromRawData(array $charactersRawData): array
+    {
+        $characters = [];
+
+        foreach ($charactersRawData as $characterData) {
+            $characters[] = $this->characterFactory->create($characterData);
+        }
+
+        return $characters;
+    }
+
+    public function getCharactersFromApi(int $limit = 1, int $offset = 0, int $id = null): array
     {
         $fullApiKey = $this->constructFullApiKey();
         $url = $this->baseUrl.'characters?'.$fullApiKey;
